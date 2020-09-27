af2630901ebbc485add250bff91c68a2b62b79fd
Edward Banfa <ebanfa@gmail.com>
2020-09-27 16:10:30 +0300

    Service factory and executor implemented

--- src/core/executor.ts
+++ src/core/executor.ts
@@ -10,19 +10,22 @@ export interface IBusinessServiceExecutor {
      * @param serviceName 
      * @param data 
      */
-    executeService<T, U>(serviceName: string, data: IRequestData<T>, store: BusinessServiceStore): IResponseData<U>
+    executeService<T, U>(serviceName: string, data: IRequestData<T>): IResponseData<U>
 }
 
 export class BusinessServiceExecutor implements IBusinessServiceExecutor {
     
+    constructor(private store: BusinessServiceStore) {
+
+    }
 
     /**
      * 
      * @param serviceName 
      * @param data 
      */
-    executeService<T, U>(serviceName: string, data: IRequestData<T>, store: BusinessServiceStore): IResponseData<U> {
-        let businessService: IBusinessService = BusinessServiceFactory.getInstance(store, serviceName, data);
+    executeService<T, U>(serviceName: string, data: IRequestData<T>): IResponseData<U> {
+        let businessService: IBusinessService = BusinessServiceFactory.getInstance(this.store, serviceName, data);
         return businessService.execute(data);
     }
 }
